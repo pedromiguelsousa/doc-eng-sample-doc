fcdcd0b25c0429a64bd9c8c5d3c5fdff3890deb1
Pedro Sousa <pedromiguelsousa@gmail.com>
2026-06-23 11:17:21 +0100

    Add tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightImageZoom from 'starlight-image-zoom';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Sample tutorial',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
 			],
+			plugins: [
+				starlightImageZoom()
+			]
 		}),
 	],
 });
